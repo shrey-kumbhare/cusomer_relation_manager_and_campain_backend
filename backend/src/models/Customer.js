e91809b4f5a116f1ed372f269b4a69903d153762
shrey-kumbhare <116801075+shrey-kumbhare@users.noreply.github.com>
2024-11-15 19:05:21 +0530

    model update

--- backend/src/models/Customer.js
+++ backend/src/models/Customer.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   totalSpend: { type: Number, default: 0 },
   numVisits: { type: Number, default: 0 },
-  lastVisitDate: { type: Date },
 });
 
-const Customer = mongoose.model('Customer', customerSchema);
+const Customer = mongoose.model("Customer", customerSchema);
 
 module.exports = Customer;
