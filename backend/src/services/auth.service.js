ee11ed4d0df263fa9aec3525dd16574bf7176570
shrey-kumbhare <116801075+shrey-kumbhare@users.noreply.github.com>
2024-11-16 17:23:55 +0530

    callback

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -9,7 +9,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL:
+        "https://shreycrmbackend.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const newUser = {
