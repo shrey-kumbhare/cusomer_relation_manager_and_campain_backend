7fd23928127bf0633a396984a566db9429024da5
shrey-kumbhare <116801075+shrey-kumbhare@users.noreply.github.com>
2024-11-16 11:51:29 +0530

    user info

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,43 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('../services/auth.service');
+const passport = require("../services/auth.service");
 
 router.get(
-  '/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 router.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
     // Successful authentication, redirect to the client application
-    res.redirect('http://localhost:3000/home');
+    res.redirect("http://localhost:3000/home");
   }
 );
 
-router.get('/status', (req, res) => {
+router.get("/status", (req, res) => {
   if (req.isAuthenticated()) {
-    res.json({ isAuthenticated: true });
+    res.json({ isAuthenticated: true, name: req.user });
   } else {
     res.json({ isAuthenticated: false });
   }
 });
 
-module.exports = router;
-
-router.post('/logout', (req, res, next) => {
+router.post("/logout", (req, res, next) => {
   req.logout((err) => {
-    if (err) { return next(err); }
-    
+    if (err) {
+      return next(err);
+    }
+
     req.session.destroy((err) => {
       if (err) {
-        return res.status(500).json({ message: 'Failed to destroy session' });
+        return res.status(500).json({ message: "Failed to destroy session" });
       }
-      
-      res.clearCookie('connect.sid', { path: '/login' });
-      res.status(200).json({ message: 'Logout successful' });
+
+      res.clearCookie("connect.sid", { path: "/login" });
+      res.status(200).json({ message: "Logout successful" });
     });
   });
 });
 
+module.exports = router;
